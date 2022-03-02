2df4a63534695585e59940b7b2586712ea8472f0
Timothy Attah <timothyattah733@gmail.com>
2022-03-02 23:32:36 +0100

    Added new colors in theme

--- client/src/themes/theme.js
+++ client/src/themes/theme.js
@@ -5,6 +5,10 @@ const theme = {
   bgDark: ' #000',
   lightBlue: ' #fcb4cd',
   gray: '#ccc',
+  lightGray: '#ddd',
+  lightYellow: '#fafafa',
+  bgColor: '#f8f9fa',
+  rgbColor: 'rgb(0, 0, 0, 0.125)',
 };
 
 export default theme;
