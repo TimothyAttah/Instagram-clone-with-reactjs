b49b41fcf6e6c142e165f8de42bc9252dfab4563
Timothy Attah <timothyattah733@gmail.com>
2022-03-03 10:12:57 +0100

    Changed style in error page

--- client/src/pages/error/style/errorPage.js
+++ client/src/pages/error/style/errorPage.js
@@ -8,8 +8,19 @@ export const StyledErrorPage = styled.div`
   text-transform: capitalize;
   font-size: 20px;
 
+  /* min-height: calc(100vh - 70px);
+  position: relative;
+  h1 {
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    font-size: 25px;
+  } */
+
   @media (max-width: 414px) {
-    font-size: 12px;
-    font-weight: bolder;
+    h1 {
+      font-size: 24px;
+    }
   }
 `;
