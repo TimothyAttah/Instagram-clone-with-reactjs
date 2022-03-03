d930189475241b36dc5a29a52f8b0bf933bf34d4
Timothy Attah <timothyattah733@gmail.com>
2022-03-03 22:23:06 +0100

    Finished redux setups

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { GlobalStyle } from './styles/global';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 ReactDOM.render(
-  <>
+  <Provider store={store}>
     <GlobalStyle />
     <App />
-  </>,
+  </Provider>,
   document.getElementById('root'),
 );
