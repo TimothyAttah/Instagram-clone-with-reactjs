81aec7e1f24c81fe3135247d83e64efbbeb0f281
Timothy Attah <timothyattah733@gmail.com>
2022-03-04 01:47:08 +0100

    Added post array in post reducer

--- client/src/redux/reducers/postReducer.js
+++ client/src/redux/reducers/postReducer.js
@@ -1,8 +1,49 @@
 import { POST_TYPES } from '../types';
+import { images } from '../../components/images';
 
 const initialState = {
   loading: false,
-  posts: [],
+  posts: [
+    {
+      content: 'This is post one',
+      images: images.PostOne,
+      likes: [1, 2, 3],
+      comments: [],
+      user: {
+        avatar: images.Alex,
+        _id: '1',
+        username: 'user01',
+      },
+      createdAt: new Date(),
+      _id: 1,
+    },
+    {
+      content: 'This is post two',
+      images: images.PostTwo,
+      likes: [1, 2],
+      comments: [],
+      user: {
+        avatar: images.Jane,
+        _id: '2',
+        username: 'user02',
+      },
+      createdAt: new Date(),
+      _id: 2,
+    },
+    {
+      content: 'This is post three',
+      images: images.PostThree,
+      likes: [],
+      comments: [],
+      user: {
+        avatar: images.Alex,
+        _id: '3',
+        username: 'user01',
+      },
+      createdAt: new Date(),
+      _id: 3,
+    },
+  ],
   result: 0,
   page: 2,
 };
